421066402935d26bcb2fd20f039e50f0a3ab3f3a
DanielDuplat <ddaniel1912@gmail.com>
2020-04-20 23:13:27 -0300

    Adding animations for the player with AnimationPlayer and AnimationTree

--- Assets/Scripts/Player/Player.gd
+++ Assets/Scripts/Player/Player.gd
@@ -3,7 +3,12 @@ extends KinematicBody2D
 const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
+
 var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input = Vector2.ZERO
@@ -14,8 +19,12 @@ func _physics_process(delta):
 	input = input.normalized()
 	
 	if input != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input)
+		animationTree.set("parameters/Run/blend_position", input)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
